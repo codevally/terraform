31f6cdaed2ff3dd81b603e169be85b866d93e97c
Mark Burke <terraformtraining@gmail.com>
2017-07-29 12:02:38 +0100

    terrafrom

--- demo.2/instance.tf
+++ demo.2/instance.tf
@@ -1,13 +1,8 @@
-provider "aws" {
-  profile = "${var.profile}"
-  region  = "${var.region}"
-}
-
 resource "aws_instance" "web-server" {
   ami           = "ami-0c2aba6c"
   instance_type = "t2.micro"
 
   tags {
-    Name = "terraformtraining"
+    Name = "terraformtraining.com"
   }
 }
--- demo.2a/instance.tf
+++ demo.2a/instance.tf
@@ -1,9 +1,3 @@
-provider "aws" {
-access_key = "${var.aws_access_key}"
-secret_key = "${var.aws_secret_key}"
-region     = "${var.region}"
-}
-
 resource "aws_instance" "web-server" {
 ami           = "ami-0c2aba6c"
 instance_type = "t2.micro"
